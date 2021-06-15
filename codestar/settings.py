577b8494460f707aa1aeb08095f64b4f43230a7a
Matt Rudge <matt.rudge@codeinstitute.net>
2021-06-15 11:10:51 +0000

    update settings

--- codestar/settings.py
+++ codestar/settings.py
@@ -162,3 +162,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
